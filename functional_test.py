2902bf70098ab5dc55005ed81709e7fe1617cada
codebot053 <codebot053@gmail.com>
2022-12-09 23:25:21 +0900

    Refactor: 기능테스트 메서드 추가 setUp, test_startapp_1, tearDown 매서드 추가

--- functional_test.py
+++ functional_test.py
@@ -2,17 +2,20 @@ from selenium import webdriver
 import unittest
 
 class TodoAppTest(unittest.TestCase):
-    
-    def test_startapp(self):
-        '''메서드의 경우 작명시 반드시 "test_"의 형태로 할것'''
-        # 할일 목록(To-do) 사이트 사용자 스토리
-
+    # 할일 목록(To-do) 사이트 사용자 스토리
+    def setUp(self):
+        '''테스트 앞서 실행될 항목'''
         # 성우는 새로운 할일 정리 사이트를 발견하고 접속해보았다.
-        browser = webdriver.Chrome('./chromedriver')
-        browser.get('http://localhost:8000')
-
-        # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다.
+        self.browser = webdriver.Chrome('./chromedriver')
+        self.browser.implicitly_wait(10) # ajax call로 dom 재구성 하는경우에 사용, 최대 10초간 더 찾아보고 종료
+        self.browser.get('http://localhost:8000')
+    
+    def test_startapp_0(self):
+        '''테스트 메서드의 경우 작명시 반드시 "test_"의 형태로 할것'''
         
+        # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다.
+        # assert 보다 assertIn 메서드 사용시 좀더 명시적이고 친절한 Error 메세지를 확인할 수 있다.
+        self.assertIn('To-do', self.browser.title)
         # 성우는 한번 할일을 추가해보기로 했다.
         # "스터디 준비하기"라고 텍스트 상자에 입력한다. (성우는 오늘도 스터디에 고통받는다)
 
@@ -28,8 +31,14 @@ class TodoAppTest(unittest.TestCase):
 
         # 해당 URL에 접속하면 성우가 만든 할일 목록이 그대로 있는 것을 확인할 수 있다.
 
+    def test_startapp_1(self):
+        '''2번째 테스트는 1번째 테스트가 실행되고난 후 다시 브라우저를 열어 실행한다.'''
+        self.assertEqual('The install worked successfully! Congratulations!', self.browser.title)
+        
+        
+    def tearDown(self):
+        '''테스트 이후 실행될 항목'''
         # 만족하고 웹사이트를 끈다.
-        browser.quit()
-
+        self.browser.quit()
 if __name__ == '__main__':
     unittest.main(warnings='ignore') # warnings='ignore'을 매개변수로 주면 실행시 에러나, 파일 로그를 무시한다.
