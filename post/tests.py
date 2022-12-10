e18263cff2608a857353535d6cc5280e0f614e25
codebot053 <codebot053@gmail.com>
2022-12-10 23:15:05 +0900

    post/test.py HomepageTest(unittest) 작성

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import resolve
+from post.views import homepage_view
 
-# Create your tests here.
+class HomepageTest(TestCase):
+    def test_root_url_match_to_homepage_view(self):
+        '''루트 URL과 매칭된 fbv 를 비교하는 단위테스트 메서드'''
+        root_url_view = resolve('/') # 해당 메서드는 매개변수로 받는 URI와 매핑된 함수 반환
+        self.assertEqual(root_url_view, homepage_view)
