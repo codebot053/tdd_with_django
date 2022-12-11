ef9ad6982810776512d91d69c8b1485c051b4116
codebot053 <codebot053@gmail.com>
2022-12-11 23:48:50 +0900

    Refactor: HompageTest 테스트 코드 수정

--- post/tests.py
+++ post/tests.py
@@ -6,4 +6,4 @@ class HomepageTest(TestCase):
     def test_root_url_match_to_homepage_view(self):
         '''루트 URL과 매칭된 fbv 를 비교하는 단위테스트 메서드'''
         root_url_view = resolve('/') # 해당 메서드는 매개변수로 받는 URI와 매핑된 함수 반환
-        self.assertEqual(root_url_view, homepage_view)
+        self.assertEqual(root_url_view.func, homepage_view)
