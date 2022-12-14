29da7f69e1ac4fcd256d7d769b537526c87a54e7
codebot053 <codebot053@gmail.com>
2022-12-14 23:40:48 +0900

    Feat: HTML content 검증 단위테스트 작성

--- post/tests.py
+++ post/tests.py
@@ -1,9 +1,17 @@
 from django.test import TestCase
 from django.urls import resolve
 from post.views import homepage_view
+from django.http import HttpRequest
 
 class HomepageTest(TestCase):
     def test_root_url_match_to_homepage_view(self):
         '''루트 URL과 매칭된 fbv 를 비교하는 단위테스트 메서드'''
         root_url_view = resolve('/') # 해당 메서드는 매개변수로 받는 URI와 매핑된 함수 반환
         self.assertEqual(root_url_view.func, homepage_view)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = homepage_view(request)
+        self.assertTrue(response.content.startswith(b'<html>')) #content 는 byte type이기에 b"str" 로 비교
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
--- post/views.py
+++ post/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
-
-def homepage_view():
-    pass
+from django.http import HttpResponse
+def homepage_view(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
